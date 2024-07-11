4525c55efb18ed07e54c6fb5c240b1f642ddaa3d
Tarunsai Simhadri <tarunsimhadri23@gmil.com>
2024-07-11 20:01:34 +0530

    [Tarunsai] Add: Python program to insert a new column values in a DataFrame.

---
+++ Pandas_programs/program21.py
@@ -0,0 +1,46 @@
+"""
+
+@Author: TarunSai
+@Date: 2024-07-09
+@Last Modified by: 
+@Last Modified time:
+@Title : Program to insert a new column values in a DataFrame.
+
+"""
+
+
+def new_column(exam_data, labels):
+
+    """
+
+    description:
+        This function is used to delete attempts column in the DataFrame.
+
+
+    parameters:
+        labels(list), exam_data(dict) : labels, exam_data that is going to frame a data frame.
+            
+    return:
+        none
+
+    """
+
+    import pandas as pd
+    
+    data = pd.DataFrame(exam_data, index=labels)
+    new_col_data = [True, False, True, False, False, True, True, False, True, False]
+    data['new_column'] = new_col_data
+    print(data)
+    
+def main():
+    import numpy as np
+    exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+                'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+                'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+                'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+    labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    new_column(exam_data, labels)
+
+
+if __name__ == "__main__":
+    main()
